56131282c22a4ba23e0f4627ba85bc21a7e6cdb7
tang6723 <tang6723@hotmail.com>
2016-09-03 12:49:51 +0800

    Tang

--- app/pages/finance/report/report.ts
+++ app/pages/finance/report/report.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CHART_DIRECTIVES } from 'angular2-highcharts';
 
 /*
   Generated class for the ReportPage page.
@@ -8,11 +9,23 @@ import { NavController } from 'ionic-angular';
   Ionic pages and navigation.
 */
 @Component({
+  directives: [CHART_DIRECTIVES],
+  styles: ['chart { display: block;}'],
+  //template: '<chart [options]="options"></chart>',
   templateUrl: 'build/pages/finance/report/report.html',
+  //directives: [CHART_DIRECTIVES]
 })
 export class ReportPage {
+  options: Object;
 
   constructor(private navCtrl: NavController) {
+    this.options = {
+      title : { text : 'simple chart' },
+      series: [{
+        data: [29.9, 71.5, 106.4, 129.2],
+      }]
+    };
+
 
   }
 
